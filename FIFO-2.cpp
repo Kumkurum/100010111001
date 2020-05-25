eb68efb5502e78ea68f70cf1df228fcbf010ecac
Kumkurum <65558964+Kumkurum@users.noreply.github.com>
2020-05-25 10:57:29 +0300

    Create FIFO-2.cpp

---
+++ FIFO-2.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+typedef int Data;
+class FIFO_1
+{
+public:
+	int maxsize;                                    //максимальное кол-во размеров очереди
+	Data* Array;                                    // одномерный массив где всё хранится
+	int item;                                       //текущий
+
+	FIFO_1(Data s) {                                //конструктор с заданием колличества элементов в циклической очереди
+		maxsize = s;
+		Array = new Data[maxsize];
+		item = 0;
+	};
+	~FIFO_1() {
+		delete[]Array;
+	};
+private:
+	void _insert(Data d) {                           //вставка элемента
+		if (item == maxsize)
+			item = 0;
+		Array[item] = d;
+		item++;
+	}
+	void _Show() {                                  //показ текущего элемента
+		if (item == maxsize) {
+			item = 0;
+		}
+		std::cout << Array[item] << std::endl;
+		item++;
+	}
+	void _Remove() {                                  //удаление текущего элемента(замена на 0)
+		if (item == maxsize)
+			item = 0;
+		Array[item] = NULL;
+		item++;
+	}
+
+	void _expansion() {                                 //расширение круга на 1
+		maxsize++;                                    //на 1 увеличим размер круга
+		int* Array_new = new int[maxsize];             //новый массив на замену старому
+		for (int i = 0; i < maxsize - 1; i++)          //по-элементное копирование
+			Array_new[i] = Array[i];
+		Array_new[maxsize - 1] = 0;                    //последний элемент - новый равен 0
+		delete[]Array;                                 //отчистка памяти
+		Array = Array_new;                             //переназначение адреса массива
+	}
+
+	void _narrowing() {                                //сужение круга на 1
+		maxsize--;
+		int* Array_new = new int[maxsize];
+		for (int i = 0; i < maxsize; i++)
+			Array_new[i] = Array[i];
+		delete[]Array;
+		Array = Array_new;
+	}
+public:                                                      //интерфейс
+	void Insert(Data d) {
+		_insert(d);
+	}
+	void Show() {
+		_Show();
+	}
+	void Remove() {
+		_Remove();
+	}
+	void Expansion() {
+		_expansion();
+	}
+	void Narrowing() {
+		_narrowing();
+	}
+};
